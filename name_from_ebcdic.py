7d05e07a677ee81edab74aade4f6be19b3b1087f
mmaquina <mmaquina@gmail.com>
2023-05-06 22:31:08 -0300

    reads line, strip spaces

--- name_from_ebcdic.py
+++ name_from_ebcdic.py
@@ -1,5 +1,17 @@
-import codecs
+#!/usr/bin/python3
 
-with open("326023_3.segy", "rb") as ebcdic:
-    ascii_txt = codecs.decode(ebcdic.read(400), "cp500")
-    print(ascii_txt)
+import codecs
+import sys 
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage:  ./name_from_ebcdic line position")
+        print("Example:  ./name_from_ebcdic 5 45")
+        exit()
+    else:
+        line = int(sys.argv[1])
+        position = int(sys.argv[2])
+
+    with open("326023_3.segy", "rb") as ebcdic:
+        ascii_txt = codecs.decode(ebcdic.read(80*40), "cp500")
+        print(len(ascii_txt[(line - 1)*80 + position - 1 : line*80].strip(' ')))
