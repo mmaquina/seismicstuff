4bc94252a942149f40d9fbdf32b8f6f3b2a148ab
mmaquina <mmaquina@gmail.com>
2023-05-07 23:20:14 -0300

    add optional parameter length

--- name_from_ebcdic.py
+++ name_from_ebcdic.py
@@ -2,16 +2,26 @@
 
 import codecs
 import sys 
+import os
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        print("Usage:  ./name_from_ebcdic line position")
+        print("Usage:  ./name_from_ebcdic line(1 is the top line) position(1 is the leftomst character position) [length(characters)]")
+        print("Example:  ./name_from_ebcdic 5 45 7")
         print("Example:  ./name_from_ebcdic 5 45")
+        print("In the latter example length is from position to the end of the line.")
         exit()
     else:
         line = int(sys.argv[1])
         position = int(sys.argv[2])
+        length = 80 - position + 1
+        if len(sys.argv) == 4:
+            length = int(sys.argv[3])
 
+
+    for path in os.walk('.'):
+        print(path)
+        
     with open("326023_3.segy", "rb") as ebcdic:
         ascii_txt = codecs.decode(ebcdic.read(80*40), "cp500")
-        print(len(ascii_txt[(line - 1)*80 + position - 1 : line*80].strip(' ')))
+        print(ascii_txt[(line - 1)*80 + position - 1 : (line - 1)*80 + position -1 + length].strip(' '))
