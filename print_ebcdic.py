7b9e28a0d9045c69f8c904102e020d367d3de802
mmaquina <mmaquina@gmail.com>
2023-05-11 22:56:11 -0300

    add utility to print ebcdic header from segy file

---
+++ print_ebcdic.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import codecs
+import sys 
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage:  ./print_ebcdic filename.sgy")
+        exit()
+
+    with open(sys.argv[1], "rb") as ebcdic:
+        ascii_txt = codecs.decode(ebcdic.read(80*40), "cp500")
+        out = ''
+        for i in range(40):
+            out += ascii_txt[i*80 : i*80 + 80] + '\n'
+        
+        print(out)
